4608eff1605781900328bb7be581c4494d68f320
CAU-LiuZiyu <liuziyu@cau.edu.cn>
2020-03-17 15:36:58 +0800

    commit for lesson four

---
+++ function.py
@@ -0,0 +1,18 @@
+class fun1():
+    #自定义fun1类
+
+    def __init__(self):
+        pass
+
+    def f1():
+        print('fun1导入成功')
+
+
+class fun2():
+    #自定义fun2类
+
+    def __init__(self):
+        pass
+
+    def f2():
+        print('fun2导入成功')
